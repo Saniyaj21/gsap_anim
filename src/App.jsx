6e319c24d991e3499c5cbd443a36ff250b7b68b4
saniyaj mallik <saniyajmallik12@gmail.com>
2024-03-09 10:37:19 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {
+	const box = useRef(null);
+
+	gsap.to(".box", {
+		scrollTrigger: {
+			trigger: ".box",
+			// toggleActions: "restart none reverse none",
+			start: "top 90%",
+			end: "top 60%",
+			// start: "top center",
+			markers: true,
+			scrub: 1,
+		},
+		x: 100,
+		rotation: 360,
+		// duration: 2,
+	});
+
+	return (
+		<>
+			<div className='container'>
+				<div ref={box} className='box'></div>
+			</div>
+		</>
+	);
+}
+
+export default App;
